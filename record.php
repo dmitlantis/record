326fc13f79f9766c2a1aaba77a657220acaf99be
dmitlantis <d.kilin@nexters.com>
2017-08-28 12:58:18 +0300

    curlify

--- record.php
+++ record.php
@@ -1,6 +1,6 @@
 <?php
 
-preg_match_all('/href="([^"]+.mp3)/i', file_get_contents('http://www.radiorecord.ru/radio/top100/rr.txt'), $files);
+preg_match_all('/href="([^"]+.mp3)/i', http('http://www.radiorecord.ru/radio/top100/rr.txt'), $files);
 set_time_limit(0);
 
 $dir = rtrim($_GET['dir'] ?? $argv[1] ?? __DIR__, '\\\/') . DIRECTORY_SEPARATOR;
@@ -23,19 +23,7 @@ if(!empty($files)) {
         $trackName = mb_substr($basename, 4);
         $newPosition = mb_substr($basename, 0, 4);
         if (!isset($trackNames[$trackName])) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($ch, CURLOPT_HEADER, false);
-            curl_setopt($ch, CURLOPT_REFERER, 'http://www.radiorecord.ru/radio/stations/?st=rr');
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-            curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
-            curl_setopt($ch, CURLOPT_URL, $file);
-            $fp = fopen($path, 'w');
-            curl_setopt($ch, CURLOPT_FILE, $fp);
-            $result = curl_exec($ch);
-            curl_close($ch);
-            fclose($fp);
+            $result = http($file,'http://www.radiorecord.ru/radio/stations/?st=rr', $path);
             echo $basename . $eol;
             if (filesize($path) < 1000) {
                 exit(file_get_contents($path));
@@ -49,4 +37,31 @@ if(!empty($files)) {
         }
     }
     echo $eol . 'Skipped:' . $eol . $skipped;
+}
+
+
+function http($url, $referrer = '', $path = '')
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    if (!empty($referrer)) {
+        curl_setopt($ch, CURLOPT_REFERER, $referrer);
+    }
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+    curl_setopt($ch, CURLOPT_URL, $url);
+    if (!empty($path)) {
+        $fp = fopen($path, 'w');
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+    } else {
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    }
+    $result = curl_exec($ch);
+    curl_close($ch);
+    if (!empty($fp)) {
+        fclose($fp);
+    }
+    return $result;
 }
